d126a6dd57a28f6f664500753e9c3edd527ba5a0
umar-abdullah22 <umar.coderzhunt@gmail.com>
2023-04-29 01:53:16 +0500

    changes

---
+++ backend/src/modules/facebook/facebook.module.ts
@@ -0,0 +1,18 @@
+import { FacebookController } from './facebook.controller';
+// twitter.module.ts
+import { Module } from '@nestjs/common';
+// import { TwitterService } from './twitter.service';
+import { User } from '../user/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
+import passport from 'passport';
+import { FacebookService } from './facebook.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User, Facebook]), AuthModule],
+  controllers: [FacebookController],
+  providers: [FacebookService],
+  // exports: [TwitterService],
+})
+export class FacebookModule {}
