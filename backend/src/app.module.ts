922660bd72023b6895156c4d5de9734726019959
umar-abdullah22 <umar.coderzhunt@gmail.com>
2023-04-29 01:44:03 +0500

    change

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -15,11 +15,6 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import mailConfig from '@config/mailConfig';
 import jwtConfig from '@config/jwtConfig';
-<<<<<<< HEAD
-=======
-import { FacebookModule } from './modules/facebook/facebook.module';
-import { LinkedinModule } from './modules/linkedin/linkedin.module';
->>>>>>> fdf1fff798ba702882f6583434807658f507373c
 
 @Module({
   imports: [
@@ -37,10 +32,6 @@ import { LinkedinModule } from './modules/linkedin/linkedin.module';
     }),
     AuthModule,
     UserModule,
-<<<<<<< HEAD
-=======
-    FacebookModule,
-    LinkedinModule,
     TwitterModule,
   ],
   controllers: [],
